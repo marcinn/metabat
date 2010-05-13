b4dbffc515abc686de965a98070f8f9c9b601298
Marcin Nowak <marcin.j.nowak@gmail.com>
2010-05-13 20:49:56 +0200

    added tests/unit module

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,17 @@
 import unittest
 
 class TestEnvironment(unittest.TestCase):
+    """
+    environment test case
+    """
     def test_initialize(self):
         try:
             import pygraph
         except ImportError:
             self.fail()
+
+
+"""
+include all unit tests
+"""
+from tests.unit import *
---
+++ tests/unit.py
@@ -0,0 +1,12 @@
+"""
+Unit tests module
+"""
+
+import unittest
+
+class MetabatCoreTestCase(unittest.TestCase):
+    """
+    core metaheuristic bat-like algorithm test case
+    """
+    pass
+
