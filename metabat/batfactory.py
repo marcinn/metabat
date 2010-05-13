707899cfc04149e0a8762324cc7076ca1d379cbb
n3wtype <marcin.matlag@gmail.com>
2010-05-14 00:01:22 +0200

    inistial BatFactory class

---
+++ metabat/batfactory.py
@@ -0,0 +1,14 @@
+class BatFactory (object):
+
+    def __init__(self, population_size, min_freq=25000, max_freq=15000):
+        """
+        Initialize bat population
+        """
+        self._min_freq = min_freq
+        self._max_freq = max_freq
+
+    def __iter(self):
+        pass
+
+    def __len(self):
+        pass
